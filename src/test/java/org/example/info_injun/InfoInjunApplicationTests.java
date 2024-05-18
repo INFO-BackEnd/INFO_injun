3d9fdf8ea5c8d4035dba69c9657d58b5b874df47
in-jun <injuninjune@gmail.com>
2024-05-18 22:25:35 +0900

    init project

---
+++ src/test/java/org/example/info_injun/InfoInjunApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.info_injun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoInjunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
